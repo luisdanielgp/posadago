7bf50682313e63e4f3592d56fb7305db19281a7f
nauseas <jadeegalicia@yahoo.com>
2017-12-01 21:02:32 -0600

    avances chiquitos

--- posadago/posadas/views.py
+++ posadago/posadas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+#from .models import Artist
+#from .serializers import ArtistsModelSerializer
+from django.http import Http404
 
-# Create your views here.
+class ListarPosadas(APIView):
+
+    def get (self, request):
+        
