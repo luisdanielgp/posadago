b8f9e37e9d5c3c6f2fd681223f99c3d19732dacf
nauseas <jadeegalicia@yahoo.com>
2017-12-01 21:37:06 -0600

    views serializer

---
+++ posadas/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Posada
+
+class PosadaModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Posada
+        fields = ('__all__')
--- posadas/views.py
+++ posadas/views.py
@@ -6,6 +6,58 @@ from .models import Posada
 from django.http import Http404
 
 class ListarPosadas(APIView):
+    '''
+    Este endpoint trae todos los artistas
+    '''
 
-    def get (self, request):
-        
+    def get(self, request):
+        posadas = Posada.objects.all()
+        serializer = PosadaModelSerializer(posadas, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PosadaModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class   PosadaIndividual(APIView):
+
+    def _get_posada(self, id):
+        try:
+            posada = Posada.objects.get(pk=id)
+            return posada
+        except Posada.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        posada = get_object_or_404(Posada, pk=id)
+        serializer = PosadaModelSerializer(posada)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        posada = self._get_posada(id)
+        serializer = PosadaModelSerializer(posada, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id):
+        posada = self._get_posada(id)
+        serializer = PosadaModelSerializer(
+            posada, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        posada = self._get_artist(id)
+        posada.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
